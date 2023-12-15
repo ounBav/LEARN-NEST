636d816b1198edc80a774811682bab2f899819c5
Oun Bav <oun.bav@igtech.asia>
2023-12-15 11:26:10 +0700

    add: implement validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
   await app.listen(3000, () => console.log('http://localhost:3000/graphql'));
 }
 bootstrap();
