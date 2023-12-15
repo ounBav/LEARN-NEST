867eee2cd7e78586c6bc3ddf996635d14594d28b
Oun Bav <oun.bav@igtech.asia>
2023-12-15 16:24:12 +0700

    use global guard

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -1,55 +1,54 @@
-import {
-  CanActivate,
-  ContextType,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { JwtService } from '@nestjs/jwt';
-import { jwtConstants } from './jwtConstants';
-import { GqlExecutionContext } from '@nestjs/graphql';
-@Injectable()
-export class GqlAuthGuard implements CanActivate {
-  constructor(
-    private reflector: Reflector,
-    private jwtService: JwtService,
-  ) {}
-  async canActivate(context: ExecutionContext) {
-    const request = this.getRequest(context);
-    const authToken = request.get('authorization') || '';
-    const [type, token] = authToken.split(' ');
-    if (type !== 'Bearer')
-      throw new UnauthorizedException('invalid provided token');
+// import {
+//   CanActivate,
+//   ContextType,
+//   ExecutionContext,
+//   Injectable,
+//   UnauthorizedException,
+// } from '@nestjs/common';
+// import { Reflector } from '@nestjs/core';
+// import { JwtService } from '@nestjs/jwt';
+// import { jwtConstants } from './jwtConstants';
+// import { GqlExecutionContext } from '@nestjs/graphql';
+// @Injectable()
+// export class GqlAuthGuard implements CanActivate {
+//   private reflector: Reflector;
+//   private jwtService: JwtService;
+//   constructor() {}
+//   async canActivate(context: ExecutionContext) {
+//     const handler = context.getHandler();
+//     const roles = this.reflector.get<string[]>('roles', handler);
+//     console.log(roles);
 
-    if (!token) {
-      throw new UnauthorizedException('Token is missing.');
-    }
+//     if (roles === undefined) return true;
 
-    try {
-      const payload = await this.jwtService.verifyAsync(token, {
-        secret: jwtConstants.secret,
-      });
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
-      request['user'] = payload;
-      console.log(payload);
-    } catch {
-      throw new UnauthorizedException();
-    }
-    return true;
-  }
-  private getContext(context: ExecutionContext): any {
-    if (context.getType<ContextType | 'graphql'>() === 'graphql') {
-      return GqlExecutionContext.create(context);
-    }
-    return context;
-  }
+//     const request = this.getRequest(context);
+//     const authToken = request.get('authorization') || '';
+//     const [type, token] = authToken.split(' ');
+//     if (type !== 'Bearer')
+//       throw new UnauthorizedException('invalid provided token');
 
-  private getRequest(context: ExecutionContext) {
-    if (context.getType<ContextType | 'graphql'>() === 'graphql') {
-      return GqlExecutionContext.create(context).getContext().req;
-    }
-    return context.switchToHttp().getRequest();
-  }
-}
+//     if (!token) {
+//       throw new UnauthorizedException('Token is missing.');
+//     }
+
+//     try {
+//       const payload = await this.jwtService.verifyAsync(token, {
+//         secret: jwtConstants.secret,
+//       });
+//       // 💡 We're assigning the payload to the request object here
+//       // so that we can access it in our route handlers
+//       request['user'] = payload;
+//       console.log(payload);
+//     } catch {
+//       throw new UnauthorizedException();
+//     }
+//     return true;
+//   }
+
+//   private getRequest(context: ExecutionContext) {
+//     if (context.getType<ContextType | 'graphql'>() === 'graphql') {
+//       return GqlExecutionContext.create(context).getContext().req;
+//     }
+//     return context.switchToHttp().getRequest();
+//   }
+// }
